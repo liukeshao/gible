ee29595dd202ee37bcc21ca588f6d1a71d60807f
kotori <liukeshao@icloud.com>
2021-07-27 17:53:29 +0800

    spring cloud consul demo

--- gible-trade/src/main/java/xyz/gosick/gible/trade/controller/OrderController.java
+++ gible-trade/src/main/java/xyz/gosick/gible/trade/controller/OrderController.java
@@ -1,16 +1,16 @@
 package xyz.gosick.gible.trade.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.gosick.gible.trade.client.UserClient;
 
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/api/trade")
 public class OrderController {
-	@Resource
+	@Autowired
 	private UserClient userClient;
 
 	@GetMapping("/test")
