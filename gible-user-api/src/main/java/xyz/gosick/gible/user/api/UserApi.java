1fcb8c05c1d98ad536f04fe0a87631238473aee4
kotori <liukeshao@icloud.com>
2021-07-28 14:19:34 +0800

    增加熔断demo

--- gible-user-api/src/main/java/xyz/gosick/gible/user/api/UserApi.java
+++ gible-user-api/src/main/java/xyz/gosick/gible/user/api/UserApi.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping(value = "/user")
 public interface UserApi {
-	@RequestMapping(method = RequestMethod.GET, value ="/users/{id}")
+	@RequestMapping(method = RequestMethod.GET, value ="/{id}")
 	String getById(@PathVariable("id") long id);
 }
--- gible-user/src/main/java/xyz/gosick/gible/user/service/UserServiceImpl.java
+++ gible-user/src/main/java/xyz/gosick/gible/user/service/UserServiceImpl.java
@@ -9,6 +9,11 @@ public class UserServiceImpl implements UserApi {
 
 	@Override
 	public String getById(long id) {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return "null";
 	}
 }
